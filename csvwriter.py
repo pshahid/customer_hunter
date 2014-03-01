336a01386ee700dba68229c4908121f7622a36bc
Paul Shahid <pshahid90@gmail.com>
2014-03-01 15:35:05 -0500

    Trying yet another way to handle the memory

--- csvwriter.py
+++ csvwriter.py
@@ -12,20 +12,22 @@ def read_from_db(fname):
     with open(fname, "w") as f:
         csv_writer = csv.writer(f, dialect='excel')
 
-        for tweet in Tweet.select():
-            row = []
-            row.append(tweet.id)
-            row.append(tweet.message.encode('ascii', 'ignore'))
-            row.append(tweet.created_date)
-            row.append(tweet.twitter_id)
-            row.append(tweet.in_reply_to_user_id)
-            row.append(tweet.in_reply_to_screen_name)
-            row.append(tweet.in_reply_to_status_id)
-            row.append(tweet.latitude)
-            row.append(tweet.longitude)
-            row.append(tweet.username)
+        for i in range(Tweet.select().count()):
+            tq = Tweet.select().where(Tweet.id == i).limit(1).execute()
+            for tweet in tq:
+                row = []
+                row.append(tweet.id)
+                row.append(tweet.message.encode('ascii', 'ignore'))
+                row.append(tweet.created_date)
+                row.append(tweet.twitter_id)
+                row.append(tweet.in_reply_to_user_id)
+                row.append(tweet.in_reply_to_screen_name)
+                row.append(tweet.in_reply_to_status_id)
+                row.append(tweet.latitude)
+                row.append(tweet.longitude)
+                row.append(tweet.username)
 
-            csv_writer.writerow(row)
+                csv_writer.writerow(row)
 
 if __name__ == "__main__":
     read_from_db("tweet.csv")
