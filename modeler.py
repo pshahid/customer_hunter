c84607a786d99df2d38be3b44706348ecf9bff45
Paul Shahid <pshahid90@gmail.com>
2014-03-04 22:08:43 -0500

    Made the code a little prettier and more modular to support future growth

--- modeler.py
+++ modeler.py
@@ -7,24 +7,15 @@ def main():
     with open('trained_and_cleansed.csv', 'r') as f:
         reader = csv.reader(f, quotechar='"')
         rows = [row for row in reader]  #Raw rows from the CSV file 
-        messages = [row[1] for row in rows[1:]] # All of the tweets (ignore the header using [1:])
-        labels = [row[0] for row in rows[1:]]   # Either 1 or 0
-        msgs_length = len(messages) - 100
 
-        #Create indices list the size of the messages list & randomize them
-        indices = [i for i in range(msgs_length)]
-        new_messages = []   #These messages will be shuffled
-        linked_labels = []  #These labels will be shuffled
-        np.random.shuffle(indices)
+        labels, tweets = randomize_datasets(rows[1:])
 
-        for index in indices:
-            new_messages.append(messages[index])
-            linked_labels.append(labels[index])
+        msgs_length = len(tweets) - 100
 
         #Vectorize our input based on the tf-idf algorithm
         vectorizer = TfidfVectorizer(input="content", ngram_range=(1, 3))   #Gonna feed it messages list; uni, bi & tri grams
-        X = vectorizer.fit_transform(new_messages[:-100])
-        y = linked_labels[:-100]
+        X = vectorizer.fit_transform(tweets[:-100])
+        y = labels[:-100]
 
         #Create a logit regression classifier
         classifier = LogisticRegression(C=1e5)
@@ -40,20 +31,41 @@ def main():
         '''
 
         #This is our test set
-        x_test = vectorizer.transform(new_messages[-100:])
+        x_test = vectorizer.transform(tweets[-100:])
 
 
         predictions = classifier.predict(x_test)
         print predictions
-        print linked_labels[-100:]
+        print labels[-100:]
 
         count = 0
         for i in range(100):
-            if predictions[i] == linked_labels[-100+i]:
+            if predictions[i] == labels[-100+i]:
                 count += 1
-            print "Predicted: " + predictions[i] + " | Actual: " + linked_labels[-100+i] 
+            print "Predicted: " + predictions[i] + " | Actual: " + labels[-100+i] 
 
         print "Total number correct out of 100: " + str(count)
+
+'''
+Randomly shuffle the given data set and return a tuple of (labels, tweets).
+
+It is assumed that the dataset given is a list of lists of length 2.
+The first index of each sub-list is assumed to be a label and the second
+index is assumed to be a tweet (string)
+'''
+def randomize_datasets(dataset):
+    np.random.seed(0)
+    np.random.shuffle(dataset)
+
+    labels = []
+    messages = []
+
+    for datum in dataset:
+        #Datum[1] = Tweet, Datum[0] = Label
+        messages.append(datum[1])
+        labels.append(datum[0])
+
+    return (labels, messages)
 
 if __name__ == "__main__":
     main()
