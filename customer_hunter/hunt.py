11c2d45a62be957d178fa96d2566502d707c4de9
Paul Shahid <pshahid90@gmail.com>
2014-08-31 20:50:20 -0400

    Added new 'hunt' file, which is how we will start running the app

---
+++ customer_hunter/hunt.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python
+
+import argparse
+import subprocess
+
+
+def start_twitter(production):
+    import jobs.twitter
+    print "Twitter job started."
+
+    if production:
+        print "Production is true"
+
+    jobs.twitter.start()
+
+def start_insta(production):
+    print "Instagram job started."
+
+    if production:
+        print "Production is true"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Main customer hunter file.")
+
+    parser.add_argument('-c', '--collect', choices=['t', 'i'])
+    parser.add_argument('-p', '--production', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.collect == 't':
+        start_twitter(args.production)
+
+    if args.collect == 'i':
+        start_insta(args.production)
+
+    print args
